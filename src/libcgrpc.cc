296eb8a4fc5adcccb27ba00a78590c9b3b607144
chengxq <815292858@qq.com>
2021-04-08 12:42:21 +0800

    add lib

---
+++ src/libcgrpc.cc
@@ -0,0 +1,19 @@
+//
+// Created by xiangqian5 on 2021/4/8.
+//
+
+#include <stdint.h>
+#include "src/diaochan/diaochan.h"
+
+extern "C" {
+    void* new_diaochan(const char* target, int32_t timeout) {
+        auto* diaochan = new CDiaochan(target, timeout);
+        return reinterpret_cast<CDiaochan*>(diaochan);
+    }
+    void delete_diaochan(CDiaochan* diaochan) {
+        delete diaochan;
+    }
+    char* recall(CDiaochan* diaochan, const char* word) {
+        return strdup(diaochan->Recall(word).c_str());
+    }
+}
