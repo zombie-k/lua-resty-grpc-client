2877489b05139fe1bb830cc816966f3027c6db4d
xiangqian5 <xiangqian5@staff.weibo.com>
2021-04-08 20:43:02 +0800

    update

--- src/diaochan/diaochan.cc
+++ src/diaochan/diaochan.cc
@@ -3,3 +3,148 @@
 //
 
 #include "diaochan.h"
+
+std::string CDiaochan::Recall(const std::string& user) {
+    // Data we are sending to the server.
+    RecallRequest request;
+    request.set_bid("123456700000");
+    request.set_uid("5014976811");
+    request.set_mid("4622872562107300");
+    request.set_channel("profile_merge_v4");
+    request.set_version("pro_s");
+    request.set_num(5);
+    FeatList *context_fl = request.mutable_context();
+    Feature *ft = context_fl->add_feat();
+    ft->set_fid("page");
+    ft->set_val("1");
+
+    ft = context_fl->add_feat();
+    ft->set_fid("uicode");
+    ft->set_val("1008");
+
+    ft = context_fl->add_feat();
+    ft->set_fid("network");
+    ft->set_val("4g");
+
+    ft = context_fl->add_feat();
+    ft->set_fid("pid");
+    ft->set_val("824");
+
+    ft = context_fl->add_feat();
+    ft->set_fid("source");
+    ft->set_val("100000");
+
+    // Container for the data we expect from the server.
+    RecallReply reply;
+
+    // Container for the data we expect from the server.
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+    std::chrono::time_point<std::chrono::system_clock> deadline =
+            std::chrono::system_clock::now() + std::chrono::milliseconds(this->timeout_);
+    context.set_deadline(deadline);
+
+    // The actual RPC.
+    Status status = stub_->Recall(&context, request, &reply);
+
+    if (status.ok()) {
+        std::string result = "";
+        generate_diaochan(&reply, result);
+        //google::protobuf::util::MessageToJsonString(reply, &reply_str);
+        //std::cout << reply_str << std::endl;
+        return result;
+    } else {
+        std::cout << status.error_code() << ": " << status.error_message()
+                  << std::endl;
+        return "RPC failed";
+    }
+}
+
+int CDiaochan::generate_diaochan(RecallReply *reply, std::string& result)
+{
+    StringBuffer buffer;
+    Writer<StringBuffer> writer(buffer);
+    writer.StartObject();
+
+    writer.Key("bid");
+    writer.String(reply->bid());
+
+    writer.Key("status");
+    writer.Int(reply->status());
+
+    writer.Key("errmsg");
+    writer.String(reply->errmsg());
+
+    // mid
+    writer.Key("mid");
+    writer.StartArray();
+    for (auto & itr : reply->mid()) {
+        writer.String(itr);
+    }
+    writer.EndArray();
+    // mid end
+
+    // mscore
+    writer.Key("mscore");
+    writer.StartArray();
+    for (float itr : reply->mscore()) {
+        writer.Double(itr);
+    }
+    writer.EndArray();
+    // mscore end
+
+    // mfeas
+    writer.Key("mfeas");
+    writer.StartArray();
+    for (const auto& mfeas : reply->mfeas()) {
+        writer.StartObject();
+        writer.Key("id");
+        writer.String(mfeas.id());
+
+        writer.Key("feat");
+        writer.StartArray();
+        for (const auto& feat : mfeas.feat()) {
+            writer.StartObject();
+            writer.Key("fid");
+            writer.String(feat.fid());
+            writer.Key("val");
+            writer.String(std::any_cast<std::string>(feat.val()));
+            writer.EndObject();
+        }
+        writer.EndArray();
+        writer.EndObject();
+    }
+    writer.EndArray();
+    // mfeas end
+
+    //otherfeas
+    FeatList otherfeas_vec = reply->otherfeas();
+    writer.Key("otherfeas");
+    writer.StartObject();
+    writer.Key("id");
+    writer.String(otherfeas_vec.id());
+    for (const auto& feat : otherfeas_vec.feat()) {
+        writer.StartObject();
+        writer.Key("fid");
+        writer.String(feat.fid());
+        writer.Key("val");
+        writer.String(std::any_cast<std::string>(feat.val()));
+        writer.EndObject();
+    }
+    writer.EndObject();
+    // otherfeas end
+
+    // debuginfo
+    writer.Key("debuginfo");
+    writer.StartArray();
+    for (auto & itr : reply->debuginfo()) {
+        writer.String(itr);
+    }
+    writer.EndArray();
+    // debuginfo end
+
+    writer.EndObject();
+    result = buffer.GetString();
+    return 0;
+}
--- src/diaochan/diaochan.h
+++ src/diaochan/diaochan.h
@@ -5,16 +5,25 @@
 #ifndef CGRPC_DIAOCHAN_H
 #define CGRPC_DIAOCHAN_H
 
+#include <iostream>
+#include <any>
 #include <grpcpp/grpcpp.h>
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
 #include "src/pb/diaochan.grpc.pb.h"
 
 
+using ::google::protobuf::RepeatedPtrField;
+using ::google::protobuf::string;
+using ::google::protobuf::RepeatedField;
 using grpc::Channel;
 using grpc::ClientContext;
 using grpc::Status;
 using diaochan::RecallRequest;
 using diaochan::RecallReply;
 using diaochan::Diaochan;
+using namespace rapidjson;
 
 class CDiaochan {
 public:
@@ -22,57 +31,9 @@ public:
     : stub_(Diaochan::NewStub(grpc::CreateChannel(target, grpc::InsecureChannelCredentials()))),timeout_(timeout)
     {}
 
-    std::string Recall(const std::string& user) {
-        std::cout << "Recall" << std::endl;
-        // Data we are sending to the server.
-        RecallRequest request;
-        request.set_bid("123456700000");
-        request.set_uid("5014976811");
-        request.set_mid("4622872562107300");
-        request.set_channel("profile_merge_v4");
-        request.set_version("pro_s");
-        request.set_num(100);
-        FeatList* context_fl = request.mutable_context();
-        Feature* ft = context_fl->add_feat();
-        ft->set_fid("page");
-        ft->set_val("1");
+    std::string Recall(const std::string& user);
 
-        ft = context_fl->add_feat();
-        ft->set_fid("uicode");
-        ft->set_val("1008");
-
-        ft = context_fl->add_feat();
-        ft->set_fid("network");
-        ft->set_val("4g");
-
-        ft = context_fl->add_feat();
-        ft->set_fid("pid");
-        ft->set_val("824");
-
-        ft = context_fl->add_feat();
-        ft->set_fid("source");
-        ft->set_val("100000");
-        // Container for the data we expect from the server.
-        RecallReply reply;
-
-        // Container for the data we expect from the server.
-        // Context for the client. It could be used to convey extra information to
-        // the server and/or tweak certain RPC behaviors.
-        ClientContext context;
-        std::chrono::time_point<std::chrono::system_clock> deadline = std::chrono::system_clock::now()+std::chrono::milliseconds(this->timeout_);
-        context.set_deadline(deadline);
-
-        // The actual RPC.
-        Status status = stub_->Recall(&context, request, &reply);
-
-        if (status.ok()) {
-            return reply.SerializeAsString();
-        } else {
-            std::cout << status.error_code() << ": " << status.error_message()
-                      << std::endl;
-            return "RPC failed";
-        }
-    }
+    int generate_diaochan(RecallReply* reply, std::string& result);
 
 private:
     std::unique_ptr<Diaochan::Stub> stub_;
